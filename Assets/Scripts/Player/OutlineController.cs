26e01a5593d0213bcebbf0b4302bf1378e75b188
Jasmine Kimball <jacksk@uoregon.edu>
2026-05-18 14:11:48 -0700

    Add interact toast when looking at outlined object

--- Assets/Scripts/Player/OutlineController.cs
+++ Assets/Scripts/Player/OutlineController.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
 using System.Collections;
+using TMPro;
 
 public class OutlineController : MonoBehaviour
 {
     [SerializeField] public Camera mainCamera;
     private Player _player;
     private Outline _currentOutline;
+    [SerializeField] public GameObject ToastBackground;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public OutlineController(Player player)
     {
         _player = player;
+    }
+
+    void Start()
+    {
+
+        ToastBackground.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -30,6 +38,8 @@ public class OutlineController : MonoBehaviour
             if (hitObject.CompareTag("InteractableDialogue") || hitObject.CompareTag("InteractablePickup"))
             {
                 hitOutline = hitObject.GetComponent<Outline>();
+                ToastBackground.gameObject.SetActive(true);
+
             }
         }
 
@@ -37,11 +47,30 @@ public class OutlineController : MonoBehaviour
         {
             if (_currentOutline != null)
                 _currentOutline.enabled = false;
+                ToastBackground.gameObject.SetActive(false);
 
             _currentOutline = hitOutline;
         }
 
         if (_currentOutline != null)
             _currentOutline.enabled = true;
+    }
+
+    void PickupItem()
+    {
+        if (_currentOutline != null)
+        {
+            // Implement item pickup logic here
+            Debug.Log("Picked up: " + _currentOutline.gameObject.name);
+        }
+    }
+
+    void DialogueItem()
+    {
+        if (_currentOutline != null)
+        {
+            // Implement dialogue logic here
+            Debug.Log("Started dialogue with: " + _currentOutline.gameObject.name);
+        }
     }
 }
